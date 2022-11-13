e52076570ea0319afda23bca92d7e0ac322f01a7
dwl21 <nggus5@naver.com>
2022-11-14 00:00:15 +0900

    refactor: clean-up style

--- webserver/main.py
+++ webserver/main.py
@@ -12,9 +12,6 @@ class Member(BaseModel):
         self.name = name
 
 
-members: dict[int, Member] = dict()
-
-
 @app.get("/")
 def root() -> dict[str, str]:
     return {"text": "Hello World"}
@@ -27,14 +24,12 @@ def create(name: str) -> Member:
 
 @app.get("/members/{member_id}")
 def read(member_id: int) -> Member:
-    return find_by_id(member_id)
+    return find_member(member_id)
 
 
 @app.get("/members")
 def read() -> dict[str, list[Member]]:
-    response: dict[str, list[Member]] = dict()
-    response["data"] = find_all()
-    return response
+    return {"data": find_all()}
 
 
 @app.put("/members/{member_id}")
@@ -47,13 +42,16 @@ def delete(member_id: int) -> None:
     delete_member(member_id)
 
 
+members: dict[int, Member] = dict()
+
+
 def save(this_name: str) -> Member:
     member: Member = Member(member_id=len(members) + 1, name=this_name)
     members[member.member_id] = member
-    return member
+    return members[member.member_id]
 
 
-def find_by_id(member_id: int) -> Member:
+def find_member(member_id: int) -> Member:
     return members[member_id]
 
 
@@ -62,7 +60,7 @@ def find_all() -> list[Member]:
 
 
 def update_member(member_id: int, name: str) -> Member:
-    member: Member = find_by_id(member_id)
+    member: Member = find_member(member_id)
     member.update_name(name)
     return member
 
